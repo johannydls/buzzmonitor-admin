84e42b46655bc64dfc459d0f9d01df81622c58d2
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-10-21 13:38:54 -0300

    Fix server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require('http');
-require('./config/database')('mongodb://buzzmonitor:buzzmonitor2018@ds055699.mlab.com:55699/buzzmonitor-admin');
+var http = require('http');
 var app = require('./config/express')();
+
+require('./config/database')('mongodb://buzzmonitor:buzzmonitor2018@ds055699.mlab.com:55699/buzzmonitor-admin');
 
 http.createServer(app).listen(app.get('port'), () => {
     console.log('Server running on localhost:',app.get('port'));
